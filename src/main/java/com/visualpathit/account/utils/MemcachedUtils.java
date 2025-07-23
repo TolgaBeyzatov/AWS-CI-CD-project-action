d4240b79a0d1dbda312cb806a4d39f196db24940
TolgaBeyzatov <tolga.beyzato@gmail.com>
2025-07-23 15:57:49 +0300

    Added memcached client details

--- src/main/java/com/visualpathit/account/utils/MemcachedUtils.java
+++ src/main/java/com/visualpathit/account/utils/MemcachedUtils.java
@@ -13,23 +13,23 @@ import com.visualpathit.account.model.User;
 
 import net.spy.memcached.MemcachedClient;
 
-public class AutoDiscoveryDemo {
+// public class AutoDiscoveryDemo {
 
-    public static void main(String[] args) throws IOException {
+//     public static void main(String[] args) throws IOException {
             
-        String configEndpoint = System.getenv("MemcachedEndpoint");
-        Integer clusterPort = 11211;
+//         String configEndpoint = System.getenv("MemcachedEndpoint");
+//         Integer clusterPort = 11211;
 
-        MemcachedClient client = new MemcachedClient(
-                                 new InetSocketAddress(configEndpoint, 
-                                                       clusterPort));       
-        // The client will connect to the other cache nodes automatically.
+//         MemcachedClient client = new MemcachedClient(
+//                                  new InetSocketAddress(configEndpoint, 
+//                                                        clusterPort));       
+//         // The client will connect to the other cache nodes automatically.
 
-        // Store a data item for an hour.  
-        // The client will decide which cache host will store this item. 
-        client.set("theKey", 3600, "This is the data value");
-    }
-}
+//         // Store a data item for an hour.  
+//         // The client will decide which cache host will store this item. 
+//         client.set("theKey", 3600, "This is the data value");
+//     }
+// }
 
 @Service
 public class MemcachedUtils {
