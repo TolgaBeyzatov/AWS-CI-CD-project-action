d4d4efe64475dc979d4db0a17c7c5d23f661aa3f
TolgaBeyzatov <tolga.beyzato@gmail.com>
2025-07-23 16:39:55 +0300

    Added memcached client details

--- src/main/java/com/visualpathit/account/utils/AutoDiscoveryDemo.java
+++ src/main/java/com/visualpathit/account/utils/AutoDiscoveryDemo.java
@@ -1,6 +1,11 @@
 package com.amazon.elasticache;
 
 import java.io.IOException;
+import java.net.InetSocketAddress;
+
+// Import the &AWS;-provided library with Auto Discovery support 
+import net.spy.memcached.MemcachedClient;  
+
 
 public class AutoDiscoveryDemo {
 
