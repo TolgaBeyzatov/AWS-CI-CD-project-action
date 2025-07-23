e14c99fd0d87e86c54b7eb8ac392bf0caaac1bcb
TolgaBeyzatov <tolga.beyzato@gmail.com>
2025-07-23 16:35:23 +0300

    Added memcached client details

--- src/main/java/com/visualpathit/account/utils/MemcachedUtils.java
+++ src/main/java/com/visualpathit/account/utils/MemcachedUtils.java
@@ -1,24 +1,3 @@
-package com.amazon.elasticache;
-
-import java.io.IOException;
-
-public class AutoDiscoveryDemo {
-
-    public static void main(String[] args) throws IOException {
-            
-        String configEndpoint = System.getenv("MemcachedEndpoint");
-        Integer clusterPort = 11211;
-
-        MemcachedClient client = new MemcachedClient(
-                                 new InetSocketAddress(configEndpoint, 
-                                                       clusterPort));       
-        // The client will connect to the other cache nodes automatically.
-
-        // Store a data item for an hour.  
-        // The client will decide which cache host will store this item. 
-        client.set("theKey", 3600, "This is the data value");
-    }
-}
 package com.visualpathit.account.utils;
 
 import java.net.InetSocketAddress;
@@ -32,8 +11,6 @@ import com.visualpathit.account.beans.Components;
 import com.visualpathit.account.model.User;
 
 import net.spy.memcached.MemcachedClient;
-
-
 @Service
 public class MemcachedUtils {
 	
